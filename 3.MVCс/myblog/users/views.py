49cbdeb3f25a02e9f069b9432279fd0b2da04ece
Petyna <petroslipchenkodg@gmail.com>
2024-12-08 10:16:12 +0200

    login view

--- 3.MVCс/myblog/users/views.py
+++ 3.MVCс/myblog/users/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect   
+from django.contrib.auth.forms import UserCreationForm, User
+from django.contrib import messages
 
 # Create your views here.
 def register_view(request):
     form = UserCreationForm()
     return render(request,"users/register.html", { "form": form })
+
+def login_view(request):
+    form = UserCreationForm()
+    return render(request,"users/login.html", { "form": form })
