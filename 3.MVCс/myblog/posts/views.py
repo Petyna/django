6243bdadd6696d3528d3bde763b396e186532819
Petyna <petroslipchenkodg@gmail.com>
2024-12-19 23:38:40 +0200

    Створення новин

--- 3.MVCс/myblog/posts/views.py
+++ 3.MVCс/myblog/posts/views.py
@@ -6,6 +6,9 @@ def posts_list(request):
     posts = Post.objects.all().order_by('-date')
     return render(request, 'posts/posts_list.html', { 'posts': posts })
 
+def post_create(request):
+    return render(request, 'posts/post_create.html')
+
 def post_page(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'posts/post_page.html', { 'post': post })
